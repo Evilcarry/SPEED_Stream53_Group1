9a9ad8bf50e971e7354f7f1fb0b940914b311a60
Evilcarry <benjhithaxx@gmail.com>
2022-10-05 00:12:57 +1300

    Trying to fix issues
    I guess it should work now

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    'http://localhost:5000'
+    'http://localhost:3500'
 ]
 
 module.exports = allowedOrigins;
--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,26 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const corsOptions = require('./config/corsOptions');
-const connectDB = require('./config/db');
-const mongoose = require('mongoose');
-const PORT = process.env.PORT || 5000;
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const path = require('path')
+const { logger, logEvents } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
+const connectDB = require('./config/db')
+const mongoose = require('mongoose')
+const PORT = process.env.PORT || 3500
 
 connectDB()
 
+app.use(logger)
+app.use(cors(corsOptions));
+app.use(express.json);
 app.use(cookieParser);
 
-app.use(cors(corsOptions));
+app.use('/', express.static(path.join(__dirname, 'public')))
 
-app.use(express.json);
-
-app.use('/', express.static(path.join(__dirname ,'public')));
-
-app.use('/', require('./routes/root'));
+app.use('/', require('./routes/root'))
 
 app.all('*', (req, res) => {
     res.status(404)
@@ -31,6 +32,8 @@ app.all('*', (req, res) => {
         res.type('txt').send('404 Not Found')
     }
 })
+
+app.use(errorHandler)
 
 mongoose.connection.once('open', () => {
     console.log('Connected to MongoDB')
