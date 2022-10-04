8c45c99f872775fcd1d2df835f9b0e2dfaf3a355
Evilcarry <benjhithaxx@gmail.com>
2022-10-04 18:23:27 +1300

    edits

--- backend/server.js
+++ backend/server.js
@@ -15,28 +15,28 @@ app.use(cookieParser);
 
 app.use(cors(corsOptions));
 
-app.use('/', express.static(path.join(__dirname, '..' ,'frontend/speed-app/public')));
+app.use(express.json);
+
+app.use('/', express.static(path.join(__dirname ,'public')));
 
 app.use('/', require('./routes/root'));
 
 app.all('*', (req, res) => {
     res.status(404)
-    if (req.accepts('html')){
-        res.sendFile(path.join(__dirname, '..', 'frontend/speed-app/public/views', '404.html'));
-    }else if (req.accepts('json')){
-        res.json({message: '404 Not Found'});
-    }else{
-        res.type('txt').send('404 Not Found');
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({ message: '404 Not Found' })
+    } else {
+        res.type('txt').send('404 Not Found')
     }
-});
+})
 
 mongoose.connection.once('open', () => {
-    console.log('Connected to mongoDB');
-    app.listen(PORT, () => {
-        console.log(`Server is running on port: ${PORT}`);
-    });
-});
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+})
 
 mongoose.connection.on('error', err => {
     console.log(err);
-});
+})
