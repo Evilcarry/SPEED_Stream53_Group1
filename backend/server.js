cd3611af75e4ec6d602ce6614f5d2d796020ca82
Evilcarry <benjhithaxx@gmail.com>
2022-10-05 14:44:13 +1300

    Bug Fixing done
    Finalized fixing the bug.
    we still have a different frontend folder

--- backend/server.js
+++ backend/server.js
@@ -21,14 +21,14 @@ app.use(express.json())
 
 app.use(cookieParser())
 
-app.use('/', express.static(path.join(__dirname, 'public')))
+app.use('/', express.static(path.join(__dirname, '..', 'frontend/public')))
 
 app.use('/', require('./routes/root'))
 
 app.all('*', (req, res) => {
     res.status(404)
     if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'views', '404.html'))
+        res.sendFile(path.join(__dirname, '..', 'frontend/views', '404.html'))
     } else if (req.accepts('json')) {
         res.json({ message: '404 Not Found' })
     } else {
