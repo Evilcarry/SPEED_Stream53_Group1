6381451f45ccdf4c862ee4a4a6ffa93e59c1c11f
Evilcarry <benjhithaxx@gmail.com>
2022-10-07 09:34:05 +1300

    changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DATABASE_URI)
+        await mongoose.connect(process.env.DATABASE_URI, { useNewUrlParser: true})
     } catch (err) {
         console.log(err)
     }
