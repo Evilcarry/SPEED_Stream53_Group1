826597d26e2ae1669d1c24f7bdc16142afafcb56
Evilcarry <benjhithaxx@gmail.com>
2022-10-13 15:07:28 +1300

    Update analisearticle.js
    Work in progress, Basically I will use the table from the searchpage but add constrains to the get method in order to only pull data that needs to be moderated

--- client/src/features/analiser/analisearticle.js
+++ client/src/features/analiser/analisearticle.js
@@ -1,12 +1,33 @@
+import Styles from "../../components/tablestyle.js";
+import Table from "../../components/SpeedTable.js";
+import tablecolumns from "../../components/tablecolumns.js";
+import Dropdown from "../../components/Dropdown.js";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
+const analisearticle = () => {
 
-const analisearticle = () => {  
-    const content = (
-        <section className="welcome">
-            <p>This is the Analiser page</p>
-        </section>
-    )
-    return content
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const result = await axios("https://speed-stream53-group1.herokuapp.com/articles");
+            setData(result.data);
+        })();
+    }, []);
+    return (
+        <div className="SearchPage">
+            <h2>SPEED Database</h2>
+            <Dropdown />
+            <Styles>
+                <Table columns={tablecolumns} data={data} />
+            </Styles>
+        </div>
+    );
 }
 
 export default analisearticle
+
+
+
+
