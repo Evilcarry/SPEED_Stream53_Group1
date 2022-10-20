21903293583d3182d70fff71266093a6bc3a7ec9
Evilcarry <benjhithaxx@gmail.com>
2022-10-20 21:15:14 +1300

    Update index.js
    Checking to see if this fixes the reloading issue

--- client/src/index.js
+++ client/src/index.js
@@ -6,11 +6,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  //<React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/*" element={<App />}></Route>
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>
+  //</React.StrictMode>
 );
