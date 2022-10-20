9a4da6197787f41bfc31633404643fc9c616a6d7
Evilcarry <benjhithaxx@gmail.com>
2022-10-20 21:20:21 +1300

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     'http://localhost:5000',
     'http://localhost:3000',
-    'https://speed-stream53-group1.herokuapp.com'
+    'https://speed-stream53-group1.herokuapp.com/'
 ]
 
 module.exports = allowedOrigins;
