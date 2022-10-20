a21e3f0cf000f65bb9b14c39e04dccbb582887a9
Evilcarry <benjhithaxx@gmail.com>
2022-10-21 08:09:38 +1300

    trying to fix the reloading issue

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.json())
 
 app.use(cookieParser())
 
-app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+app.use('/', express.static(path.resolve(__dirname, 'client', 'build')))
 
 app.use('/', require('./routes/root'))
 app.use('/articles', require('./routes/articleRoutes'))
