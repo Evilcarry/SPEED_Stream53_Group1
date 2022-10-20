ee33f315db042ff45b6ff8b0c5d6b089c7cf1ebf
jenniferty <bqd3000@autuni.ac.nz>
2022-10-20 20:47:16 +1300

    can add articletext to database

--- client/src/components/AnalystForm.js
+++ client/src/components/AnalystForm.js
@@ -1,21 +1,33 @@
 import { useState } from 'react';
+import axios from 'axios'
 
 function AnalystForm(row) {
   const [extract, setExtract] = useState("");
+  const client = axios.create({
+    baseURL: "https://speed-stream53-group1.herokuapp.com/analiser"
+})
 
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    alert(row.doi + row.title + extract);
+  function handleSubmit(title, doi, articleText) {
+    try {
+        client
+        .patch('', {
+            title: title,
+            doi: doi,
+            articleText: articleText
+        })       
+    }catch (err){
+        console.log(err.message)
+    }
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(row.title, row.doi, extract)}>
         <textarea 
           value={extract}
           onChange={(e) => setExtract(e.target.value)}
         />
-      <input type="submit" />
+      <input disabled={!extract} type="submit" />
     </form>
   )
 }
